20ab52c5eb13620886b407b31c565450e8f125d3
Ellisthion <ellisthion@gmail.com>
2023-01-12 22:31:01 +1100

    Gears and plates

---
+++ kubejs/server_scripts/gears.js
@@ -0,0 +1,19 @@
+ServerEvents.recipes(event => {
+    // Add missing Immersive Engineering press recipes
+    let materials = [
+        'netherite',
+        'signalum',
+        'lumium',
+        'enderium',
+    ];
+
+    for (let material of materials) {
+        event.custom({
+            type: "immersiveengineering:metal_press",
+            energy: 2400,
+            input: { "tag": `forge:ingots/${material}` },
+            mold: "immersiveengineering:mold_gear",
+            result: { "tag": `forge:gears/${material}` },
+        });
+    }
+});
---
+++ kubejs/server_scripts/progression_recipes.js
@@ -0,0 +1,13 @@
+/*
+Most progression recipes are done in datapacks
+
+- Botania: Petal apothecary requires mana diamond instead of flower petal
+- Ars: Ars book made with manasteel tools
+- Hex: Staves made with livingwood twigs
+- Hex: Cypher replace one ingot with with manasteel, trinket with elementium, artifact with terrasteel
+- Thermal: Remove crafting recipe for gears, only made via ImmEng automation
+- Flux: Flux Core replace Eye of Ender with Bucket of Resonent Ender
+*/
+
+ServerEvents.recipes(event => {
+})  
