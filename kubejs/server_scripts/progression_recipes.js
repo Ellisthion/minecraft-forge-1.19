cf5adbd68d01fa5dc12c399e8461c741d7616ac6
Ellisthion <ellisthion@gmail.com>
2023-01-12 23:01:09 +1100

    Flux core progression

--- kubejs/server_scripts/progression_recipes.js
+++ kubejs/server_scripts/progression_recipes.js
@@ -6,8 +6,10 @@ Most progression recipes are done in datapacks
 - Hex: Staves made with livingwood twigs
 - Hex: Cypher replace one ingot with with manasteel, trinket with elementium, artifact with terrasteel
 - Thermal: Remove crafting recipe for gears, only made via ImmEng automation
-- Flux: Flux Core replace Eye of Ender with Bucket of Resonent Ender
+- Flux: Flux Core replace Eye of Ender with Enderium coin
+
 */
 
 ServerEvents.recipes(event => {
+    event.replaceInput({ id: 'fluxnetworks:fluxcore', type: 'minecraft:crafting_shaped' }, 'minecraft:ender_eye', '#forge:coins/enderium')
 })  
