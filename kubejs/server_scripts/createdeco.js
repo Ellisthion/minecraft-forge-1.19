81da484438a18e51eeceb036ab25f15c90b8f790
Ellisthion <ellisthion@gmail.com>
2023-02-07 14:51:38 +1100

    Fix recipe conflict on copper and gold bars

--- kubejs/server_scripts/createdeco.js
+++ kubejs/server_scripts/createdeco.js
@@ -1,8 +1,13 @@
 ServerEvents.recipes(event => {
 	event.remove({ id: 'createdeco:copper_bars' })
+    event.remove({ id: 'createdeco:gold_bars' })
     event.remove({ id: 'createdeco:copper_door' })
 
     // Allow crafting back and forth between copper door variants
     event.shapeless('createdeco:copper_door', ['copperized:copper_door'])
     event.shapeless('copperized:copper_door', ['createdeco:copper_door'])
+
+    // Allow crafting create deco bars from quark/copperized bars
+    event.shapeless('2x createdeco:copper_bars', ['2x copperized:copper_bars'])
+    event.shapeless('2x createdeco:gold_bars', ['2x quark:gold_bars'])
 })
