3b112b0c2ee5390cddea07458114e61c6dfe027e
Ellisthion <ellisthion@gmail.com>
2023-02-07 15:48:38 +1100

    Re-enable coffee... with more bugfixes

--- kubejs/client_scripts/crops.js
+++ kubejs/client_scripts/crops.js
@@ -1,6 +1,3 @@
 JEIEvents.hideItems(event => {
 	event.hide('sushigocrafting:rice_seeds')
-    event.hide('createcafe:coffee_beans')
-    event.hide('createcafe:coffee_fruit')
-    event.hide('createcafe:roasted_coffee')
 })
--- kubejs/server_scripts/crops.js
+++ kubejs/server_scripts/crops.js
@@ -7,8 +7,4 @@ LootJS.modifiers((event) => {
     // Remove Sushi Go rice
     event.addBlockLootModifier("minecraft:grass").removeLoot("sushigocrafting:rice_seeds");
     event.addBlockLootModifier("minecraft:tall_grass").removeLoot("sushigocrafting:rice_seeds");
-
-    // Remove Create Cafe coffee, since it's unused
-    event.addBlockLootModifier("minecraft:grass").removeLoot("createcafe:coffee_beans");
-    event.addBlockLootModifier("minecraft:tall_grass").removeLoot("createcafe:coffee_beans");
 });
