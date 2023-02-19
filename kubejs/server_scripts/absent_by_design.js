898d4f7993e370b263ba09439a841c2de5569abc
Ellisthion <ellisthion@gmail.com>
2023-02-19 21:52:19 +1100

    Fix wool slab recipe conflict

--- kubejs/server_scripts/absent_by_design.js
+++ kubejs/server_scripts/absent_by_design.js
@@ -13,4 +13,41 @@ ServerEvents.recipes(event => {
         event.remove({ input: id, mod: 'absentbydesign' });
         event.remove({ output: id, mod: 'absentbydesign' });
     }
+
+
+    // Wool slabs conflict with sleeping bags
+
+    // [slab, wool]
+    let colours = [
+        ['black', 'black'],
+        ['blue', 'blue'],
+        ['brown', 'brown'],
+        ['cyan', 'cyan'],
+        ['gray', 'gray'],
+        ['green', 'green'],
+        ['light_blue', 'light_blue'],
+        ['lime', 'lime'],
+        ['magenta', 'magenta'],
+        ['orange', 'orange'],
+        ['pink', 'pink'],
+        ['purple', 'purple'],
+        ['red', 'red'],
+        ['silver', 'light_gray'],
+        ['white', 'white'],
+        ['yellow', 'yellow'],
+    ]
+
+    for (let colour of colours) {
+        let slab = `absentbydesign:slab_wool_${colour[0]}`
+        let wool = `minecraft:${colour[1]}_wool`
+
+        event.remove({ id: slab });
+        event.shaped(`12x ${slab}`, [
+            '   ',
+            'WWW',
+            'WWW'
+        ], {
+            W: wool,
+        })
+    }
 })
